7e4f9fe7ef8ac3a2645f5dae722ab2804a956ca0
octaltree <octaltree@gmail.com>
2017-02-04 02:53:44 +0900

    modulize

--- aboutbmarks.js
+++ aboutbmarks.js
@@ -15,29 +15,45 @@ var INFO = xml`
     .getService(Ci.nsINavBookmarksService);
   const hs = Components.classes["@mozilla.org/browser/nav-history-service;1"]
     .getService(Components.interfaces.nsINavHistoryService);
+  var bookmark = { // {{{
+    bkm:
+      Cc["@mozilla.org/browser/nav-bookmarks-service;1"]
+      .getService(Ci.nsINavBookmarksService),
+    hst:
+      Components.classes["@mozilla.org/browser/nav-history-service;1"]
+      .getService(Components.interfaces.nsINavHistoryService),
+    processContents: (folder, f) => {
+      const query = folder => {
+        var tmp = bookmark.hst.getNewQuery();
+        tmp.setFolders([folder], 1);
+        return tmp;
+      };
+      var r = bookmark.hst
+        .executeQuery(query(folder), hs.getNewQueryOptions()).root;
+      r.containerOpen = true;
+      const range = n => Array.from({length: n}, (v, k) => k);
+      const children = range(r.childCount).map(x => r.getChild(x));
+      const res = f(children);
+      r.containerOpen = false;
+      return res;
+    },
+    //allFolders: folder => {
+
+    //}
+    allFolders: folder => bookmark.processContents(folder, bs => {
+      const fs = bs.filter(x => x.type == x.RESULT_TYPE_FOLDER);
+      const childfolders = fs.filter(c => c.type == c.RESULT_TYPE_FOLDER);
+      const deep = childfolders.map(c => bookmark.allFolders(c.itemId));
+      return {id: folder, children: deep};
+    })}; // }}}
 
   function main(){
     clearPage(); // TODO 専用ページをつくりopen, tabopenできるようにしたい
     logger.init();
-    const allfolders = recFolders(bkms.placesRoot);
-    logger.dump(allfolders);
+    logger.dump(bookmark.bkm);
+    logger.dump(bookmark.allFolders(bookmark.bkm.placesRoot));
   }
 
-  function recFolders(folder){ // {{{
-    const query = folder => {
-      var tmp = hs.getNewQuery();
-      tmp.setFolders([folder], 1);
-      return tmp;
-    };
-    var r = hs.executeQuery(query(folder), hs.getNewQueryOptions()).root;
-    r.containerOpen = true;
-    const range = n => Array.from({length: n}, (v, k) => k);
-    const children = range(r.childCount).map(x => r.getChild(x));
-    const childfolders = children.filter(c => c.type == c.RESULT_TYPE_FOLDER);
-    const deep = childfolders.map(c => recFolders(c.itemId));
-    r.containerOpen = false;
-    return {id: r.itemId, children: deep};
-  } // }}}
   function clearPage(){ // {{{
     const d = content.document;
     const p = d.documentElement.parentNode;
